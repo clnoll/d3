f95160fdc8a74281274bfad44d2568efdf75cee4
clnoll <noll.catherine@gmail.com>
2014-09-24 21:06:23 -0700

    readme and update for wind speed

---
+++ README.txt
@@ -0,0 +1,8 @@
+Deployed at:
+
+http://vast-mesa-9447.herokuapp.com/.
+
+
+Simple scatterplot showing wind chills in Chicago, using Yahoo's weather API.  App is set to query the API every "2 hours" (actually every 3 seconds for the demo purposes).
+
+User can update the scatterplot to show new data by clicking the "update" button.  Over a period of time you can see how the wind chill varies at different times of day; the color and size of the points corresponds with the temperature.
--- lib/dashboard/get_temps.rb
+++ lib/dashboard/get_temps.rb
@@ -1,40 +1,9 @@
-# require 'open-uri'
-# require 'nokogiri'
 
 class Dashboard::GetTemps
 
   def initialize
-    # @str = str
-    # @events = []
-    # @uri = URI.parse(@str)
-    # @host = @uri.host
-    # @split_str = @str.split('/')
-    # @base = URI.parse("http://#{@host}")
   end
 
   def get_temps
-    # @str
-    # urls = []
-
-    # # Open the page associated with the base url of the event
-    # html_doc = Nokogiri::HTML(open(@base))
-
-    # # Parse through to identify urls, and add to urls array
-    # html_doc.xpath('//a/@href').each do |i|
-    #     urls << i.content
-    # end
-
-    # # Pass each url through the check_urls method to determine whether they may be valid events
-    # urls.each do |i|
-    #     check_urls(i)
-    # end
-
-    # # If too few events were returned, crawl through the event pages to identify more
-    # if @events.length < 10
-    #     crawl_events(@uri)
-    # end
-
-    # # Return the array of event urls
-    # @events
   end
 end
